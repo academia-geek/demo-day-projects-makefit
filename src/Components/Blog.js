5c110cb7eeab920af8f5a40a7a5c84909462b5a0
CarlinGebyte <91474067+CarlinGebyte@users.noreply.github.com>
2022-04-18 18:56:58 -0500

    Creating Search component to blog entries, solving a little bug in Blog.js component

--- src/Components/Blog.js
+++ src/Components/Blog.js
@@ -2,8 +2,8 @@ import React from "react";
 import { useEffect } from "react";
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
-import { listAsync } from "../Redux/actions/blogActions";
+import { useNavigate, useParams } from "react-router-dom";
+import { deleteAsync, listAsync } from "../Redux/actions/blogActions";
 import EditBlog from "./EditBlog";
 
 function Blog() {
@@ -20,13 +20,42 @@ function Blog() {
 
   const navigate = useNavigate();
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const param = e.target.search.value;
+    navigate(`/blog/search/${param}`);
+  };
+
   const edit = (blogEntry) => {
     setEditModal(blogEntry);
     setModal(true);
   };
+
+  const deletePost = (id) => {
+    dispacth(deleteAsync(id));
+  };
+
   return (
     <div>
-      <div></div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="search"
+          placeholder="Ingresa tu búsqueda en el blog"
+        />
+        <button type="submit">Buscar</button>
+      </form>
+      <div>
+        {posts.map((post) => (
+          <div key={post.id}>
+            <img src={post.image} alt="blog" />
+            <h1>{post.title}</h1>
+            <p>{post.body}</p>
+            <button onClick={() => edit(post)}>Edit</button>
+            <button onClick={() => deletePost(post.id)}>Delete</button>
+          </div>
+        ))}
+      </div>
       {modal === true ? <EditBlog modal={editModal} close={setModal} /> : ""}
     </div>
   );
