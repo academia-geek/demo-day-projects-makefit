627210c04e35bbe24b834da8f5cdcd7cb2c640e0
Hipns1 <jesudpf21@hotmail.com>
2022-04-18 20:13:13 -0500

    c

--- src/Components/NavBarUser.js
+++ src/Components/NavBarUser.js
@@ -4,7 +4,9 @@ import { Link } from 'react-router-dom'
 const NavBarUser = () => {
     return (
         <div>
+            <Link to="/home">Home</Link>
             <Link to="/blog">Blog</Link>
+            <Link to="/blog/add">Agregar entrada el blog</Link>
         </div>
     )
 }
