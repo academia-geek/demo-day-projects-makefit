267fd3771c10b8727dbe704c8db2664e3269002c
Hipns1 <jesudpf21@hotmail.com>
2022-04-18 20:16:50 -0500

    push

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,24 +1,14 @@
 import CardRecipe from './CardRecipe'
 import { useGetRecipes } from '../Hooks/useGetRecipes'
-import { useDispatch } from 'react-redux'
-import { useNavigate } from 'react-router-dom'
-import { logoutAsync } from '../Redux/actions/actionLogin'
 import '../Styles/cardRecipe.css'
 import { SearchRecipes } from './SearchRecipes'
 
 export function Dashboard() {
 	const { recipes } = useGetRecipes()
-	const dispatch = useDispatch()
-	const navigate = useNavigate()
-	//cerrar sesion
-	const handleLogout = () => {
-		dispatch(logoutAsync())
-		navigate('/landingpage')
-	}
+	
 
 	return (
 		<main>
-			<button onClick={() => handleLogout()}>LogOut</button>
 			<h1>Macros</h1>
 			<SearchRecipes />
 
--- src/Components/NavBarUser.js
+++ src/Components/NavBarUser.js
@@ -1,12 +1,22 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+import { Link, useNavigate } from 'react-router-dom'
+import { logoutAsync } from '../Redux/actions/actionLogin'
 
 const NavBarUser = () => {
+    const dispatch = useDispatch()
+	const navigate = useNavigate()
+	//cerrar sesion
+	const handleLogout = () => {
+		dispatch(logoutAsync())
+		navigate('/landingpage')
+	}
     return (
         <div>
             <Link to="/home">Home</Link>
             <Link to="/blog">Blog</Link>
             <Link to="/blog/add">Agregar entrada el blog</Link>
+            <button style={{width: "150px"}}onClick={() => handleLogout()}>LogOut</button>
         </div>
     )
 }
