07163986309200ab4c5e4d4fb81a67ad4508bc9b
Hipns1 <jesudpf21@hotmail.com>
2022-04-19 00:36:14 -0500

    optimizacion de busqueda

--- src/Components/SearchRecipes.js
+++ src/Components/SearchRecipes.js
@@ -1,16 +1,16 @@
-import { Link } from 'react-router-dom'
 import { useGetQuery } from '../Hooks/useGetQuery'
+import { useNavigate } from "react-router-dom";
 
 export function SearchRecipes() {
 	const { queryRef, query, getQuery } = useGetQuery()
-
+	const navigate = useNavigate()
 	return (
 		<div className='App'>
 			<section className='controls'>
-				<form>
+				<form onSubmit={() => navigate(`/results/${query}`)}>
 					<input onChange={getQuery} type='text' placeholder='Query ' ref={queryRef} />
 					<button>
-						<Link to={`/results/${query}`}><i className="fa-solid fa-magnifying-glass"></i></Link>
+						<i className="fa-solid fa-magnifying-glass"></i>
 					</button>
 				</form>
 			</section>
