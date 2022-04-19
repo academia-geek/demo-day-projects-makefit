5dca1f33d10f683c99e1025b944067b9852c8a61
Hipns1 <jesudpf21@hotmail.com>
2022-04-18 19:59:45 -0500

    Rutas

---
+++ src/Components/NavBarUser.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const NavBarUser = () => {
+    return (
+        <div>
+            <Link to="/blog">Blog</Link>
+        </div>
+    )
+}
+
+export default NavBarUser
