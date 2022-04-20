75e4ad5866ac1caa1a4f140893c765f02aa1cfdb
Yessith Estiven Herrera Sanjuan <yessith.hs@gmail.com>
2022-04-20 11:31:19 -0500

    fix bug

--- src/utils/apiUrls.js
+++ src/utils/apiUrls.js
@@ -1,4 +1,5 @@
-const apiKey = '5d0fd108edef4233b88d980e783c9a45'
+// const apiKey = '5d0fd108edef4233b88d980e783c9a45'
+const apiKey = 'cb1c464d94f142c08b156c5beddade8b'
 const baseUrl = 'https://api.spoonacular.com/recipes/'
 
 const randomRecipes = `${baseUrl}random?number=5&apiKey=${apiKey} `
