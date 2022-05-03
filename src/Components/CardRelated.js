046c4950510104cee1ecfbaf40acacdfbf367989
Yessith Estiven Herrera Sanjuan <yessith.hs@gmail.com>
2022-05-03 15:08:30 -0500

    styles analyze img

--- src/Components/CardRelated.js
+++ src/Components/CardRelated.js
@@ -1,18 +1,19 @@
 import { Link } from 'react-router-dom'
+import styles from '../Styles/UploadPicture/cardRelated.module.scss'
 
-export default function CardRelated({ recipes = [] }) {
+export function CardRelated({ recipes = [] }) {
 	return (
 		<>
 			{recipes.map((recipe) => (
-				<article key={recipe.id}>
-					<Link to={`/details/${recipe.id}`}>
-						<h3>{recipe.title}</h3>
-						<figure>
+				<article key={recipe.id} className={styles.card}>
+					<Link to={`/details/${recipe.id}`} className={styles.card_item}>
+						<figure className={styles.card_img}>
 							<img
 								src={`https://spoonacular.com/recipeImages/${recipe.id}-240x150.jpg`}
 								alt={recipe.title}
 							/>
 						</figure>
+						<h3 className={styles.card_title}>{recipe.title}</h3>
 					</Link>
 				</article>
 			))}
