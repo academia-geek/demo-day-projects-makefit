775334746d6278e8975d5ea63cf9d0cb297fe5f5
Yessith Estiven Herrera Sanjuan <yessith.hs@gmail.com>
2022-05-04 11:13:33 -0500

    Add Type of diet

--- src/Components/DetailsRecipe.js
+++ src/Components/DetailsRecipe.js
@@ -9,9 +9,11 @@ import { Spinner } from './Spinner'
 import { Instructions } from './Instructions'
 
 import styles from '../Styles/Details/details.module.scss'
+import { TypeOfDiet } from './TypeOfDiet'
 
 export function DetailsRecipe() {
 	const { results } = useGetDetailsRecipe()
+	console.log('🚀 ~ file: DetailsRecipe.js ~ line 15 ~ DetailsRecipe ~ results', results)
 	const { isNearScreen, fromRef } = useIntersectionObserver()
 	const NutritionalInfo = lazy(() => import('./NutritionalInfo'))
 
@@ -33,6 +35,16 @@ export function DetailsRecipe() {
 							<span className={styles.timeBox_value}>{results.servings} persons</span>
 						</div>
 					</div>
+				</div>
+			</section>
+			<section className={styles.diet}>
+				<h2>Type Of Diet</h2>
+				<div className={styles.diet_grid}>
+					<TypeOfDiet diet='Vegan' validate={results.vegan} />
+					<TypeOfDiet diet='Vegetarian' validate={results.vegetarian} />
+					<TypeOfDiet diet='Gluten Free' validate={results.glutenFree} />
+					<TypeOfDiet diet='Very Healthy' validate={results.veryHealthy} />
+					<TypeOfDiet diet='Dairy Free' validate={results.dairyFree} />
 				</div>
 			</section>
 			<section className={styles.details_recipe}>
--- src/utils/apiUrls.js
+++ src/utils/apiUrls.js
@@ -1,6 +1,6 @@
-// const apiKey = '612fae429be246b6aa24a20de8c595df'
+const apiKey = '612fae429be246b6aa24a20de8c595df'
 // const apiKey = 'a6b16be1e0354caeaba5899362be1d5a'
-const apiKey = 'f38898caa70c4d1a9a9b14589fd8e362'
+// const apiKey = 'f38898caa70c4d1a9a9b14589fd8e362'
 // const apiKey = '7fc319634e1e4f95ab424e644529511c'
 // const apiKey = 'cb1c464d94f142c08b156c5beddade8b'
 // const apiKey = '5d0fd108edef4233b88d980e783c9a45'
