36de8a7f270de0d96a74f220ce4fd7b891e2b82b
Hipns1 <jesudpf21@hotmail.com>
2022-05-05 03:22:27 -0500

    Mejora de estilos de cartas y pagina principal

--- src/Components/CardRelated.js
+++ src/Components/CardRelated.js
@@ -3,20 +3,41 @@ import styles from '../Styles/UploadPicture/cardRelated.module.scss'
 
 export function CardRelated({ recipes = [] }) {
 	return (
-		<>
-			{recipes.map((recipe) => (
-				<article key={recipe.id} className={styles.card}>
-					<Link to={`/details/${recipe.id}`} className={styles.card_item}>
-						<figure className={styles.card_img}>
-							<img
-								src={`https://spoonacular.com/recipeImages/${recipe.id}-240x150.jpg`}
-								alt={recipe.title}
-							/>
-						</figure>
-						<h3 className={styles.card_title}>{recipe.title}</h3>
-					</Link>
-				</article>
-			))}
-		</>
+		<div className={styles.related_container}>
+			{
+				recipes.map((recipe, index) => (
+					<div
+						className={styles.related_card}
+						key={index} >
+						<Link to={`/details/${recipe.id}`}>
+							<div className={styles.related_card__img}>
+								<img
+									loading='lazy'
+									src={`https://spoonacular.com/recipeImages/${recipe.id}-240x150.jpg`}
+									alt={recipe.title} />
+							</div>
+						</Link>
+						<Link to={`/details/${recipe.id}`}>
+							<h1>
+								{recipe.title}
+							</h1>
+						</Link>
+					</div>
+				))
+			}
+		</div >
 	)
 }
+
+
+{/* <article key={recipe.id} className={styles.card}>
+	<Link to={`/details/${recipe.id}`} className={styles.card_item}>
+		<figure className={styles.card_img}>
+			<img
+				src={`https://spoonacular.com/recipeImages/${recipe.id}-240x150.jpg`}
+				alt={recipe.title}
+			/>
+		</figure>
+		<h3 className={styles.card_title}>{recipe.title}</h3>
+	</Link>
+</article> */}
--- src/Components/SearchRecipes.js
+++ src/Components/SearchRecipes.js
@@ -1,5 +1,6 @@
-import { useGetQuery } from '../Hooks/useGetQuery'
-import { useNavigate } from 'react-router-dom'
+import { useGetQuery } from '../Hooks/useGetQuery';
+import { useNavigate } from 'react-router-dom';
+import styles from '../Styles/Dashboard/Dashboard.module.scss';
 
 const SearchRecipes = () => {
 	const { queryRef, query, getQuery } = useGetQuery()
@@ -7,12 +8,14 @@ const SearchRecipes = () => {
 	const keyword = query.toLowerCase()
 
 	return (
-		<section className='controls'>
-			<div>
+		<section className={styles.search_container}>
+			<div className={styles.search_text}>
 				<h1>Discover Recipes</h1>
 				<p>Get the best recipe every day of the week with our daily picks</p>
 			</div>
-			<form onSubmit={() => navigate(`/results/${keyword}`)}>
+			<form 
+				className={styles.search_form}
+				onSubmit={() => navigate(`/results/${keyword}`)}>
 				<input onChange={getQuery} type='text' placeholder='Search for recipes ' ref={queryRef} />
 				<button>
 					<i className='fa-solid fa-magnifying-glass'></i>
